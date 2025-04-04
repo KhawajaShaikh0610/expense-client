91c5c11357a20e72ecdb6b5b58bbe0f24646e392
KhawajaShaikh0610 <shaikhkhawaja0610@gmail.com>
2025-04-04 22:41:43 +0530

    admin api

--- src/redux/api/adminApi.js
+++ src/redux/api/adminApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const adminApi = createApi({
   reducerPath: "adminApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api/admin",
+    baseUrl: "http://localhost:4000/admin",
     prepareHeaders: (headers) => {
       const token = localStorage.getItem("adminToken");
       if (token) {
