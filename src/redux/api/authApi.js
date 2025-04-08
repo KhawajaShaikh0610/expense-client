f18467d695e482346435326956439fa32c0bf8af
asadshaikh41 <87890728+asadshaikh41@users.noreply.github.com>
2025-04-08 18:10:23 +0530

    code

--- src/redux/api/authApi.js
+++ src/redux/api/authApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const authApi = createApi({
   reducerPath: "authApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:4000/user" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/user" }),
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (credentials) => ({
